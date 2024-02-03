fd3f1fcb3dd0a6025989c405b72b50e55292c274
bhilleri <benjaminh@live.fr>
2024-02-03 22:33:46 +0100

    Ajout des options dans la commande
    La commande doit maintenant être appelé par les options --regex et --patern
    La libraire Microsoft.Extensions.Configuration.CommandLine a été ajoutée pour gérer les options
    Le Readme a été mis à jour en conséquence

--- Program.cs
+++ Program.cs
@@ -1,24 +1,69 @@
 ﻿using System.IO.Enumeration;
 using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml.Bibliography;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.Extensions.Configuration;
 
-// Permet pour le moment de rechercher une regex dans un fichier
-// Taper dotnet run ./OfficeFinder <pattern> <File>
-string pattern = Environment.GetCommandLineArgs()[2];
-string fileName = Environment.GetCommandLineArgs()[3];
-
-
-using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(fileName, false)){
-    MainDocumentPart mainDocument= wordDoc.MainDocumentPart;
-    Body body = mainDocument.Document.Body;
-    string text = body.InnerText;
-
-    
-    RegexOptions options = RegexOptions.Multiline;
-    
-    foreach (Match m in Regex.Matches(text, pattern, options))
+// Permet de rechercher une regex dans un fichier
+// Taper OfficeFinder --regex <pattern> --path <File>
+public class Program
+{
+    public static void Main(string[] args)
     {
-        Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        // Get arguments
+        var switchMappings = new Dictionary<string, string>()
+    {
+        { "--regex", "regex" },
+        { "-r", "regexAbbreviation"},
+        { "--path", "path" },
+        { "-p", "pathAbbreviation"},
+    };
+        var builder = new ConfigurationBuilder();
+        builder.AddCommandLine(args, switchMappings);
+
+        var config = builder.Build();
+
+        // Retrieve fileName and the pattern for the regex
+        string? fileName = config["path"] ?? config["pathAbbreviation"];
+        string? pattern = config["regex"] ?? config["regexAbbreviation"];
+
+        // Check if parameters are not null
+        if (pattern is null || fileName is null)
+        {
+            Console.WriteLine("Pour utiliser la commande taper : OfficeFinder <pattern> <file>");
+            throw new ArgumentException("Les arguments de la commande ne sont pas correctement saisies");
+        }
+
+
+        // Check if file exist
+        if (File.Exists(fileName) == false)
+        {
+            Console.WriteLine($"Le fichier {fileName} n'a pas été trouvé");
+            throw new FileNotFoundException($"Fichier {fileName} inexistant");
+        }
+
+        // Search on the word file
+        using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(fileName, false))
+        {
+
+            if (wordDoc is null)
+            {
+                throw new ArgumentNullException(nameof(wordDoc));
+            }
+
+            MainDocumentPart mainDocument = wordDoc.MainDocumentPart ?? wordDoc.AddMainDocumentPart();
+            Body body = mainDocument.Document.Body ?? new Body();
+
+            RegexOptions options = RegexOptions.Multiline;
+            MatchCollection AllMatchs = Regex.Matches(body.InnerText, pattern, options);
+            if (AllMatchs.Count > 0)
+                Console.WriteLine($"{fileName} : {AllMatchs.Count}");
+            else
+                Console.WriteLine($"Aucune occurence trouvé dans le fichier {fileName} avec le pattern {pattern}");
+        }
+
     }
 }
+
+
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # OffcieFinder
 Permet de rechercher des informations dans plusieurs fichiers office
+
+## Utilisation de l'outil
+taper la commande OfficeFinder.exe --regex <regex> --path <path>
+ou : OfficeFinder.exe -r <regex> -p <path>
+
+<regex> : Regex définissant l'élément recherché dans le fichier
+<path> : Chemin vers le fichier cible
+
+Pour créer des regex vous pouvez utiliser le site https://regex101.com/ 
+Pour le moment la recherche est uniquement multi-ligne. Il n'est pas encore possible de venir définir des options supplémentaires dans la regex.
+Si vous recherez un mot vous pouvez directement le tapez.
+Si vous tapez une phrase ou que votre recherche comprend des espaces, il faut rajouter des guillemets autours autours de celle-ci
+
+## example
+
+- recherche de mot :
+OfficeFinder.exe bonjour ./documentWord.docx
+
+- recherche de phrase :
+OfficeFinder.exe "Ceci est ma recherche" ./documentWord.docx
+
+- recherche avec une regex plus compliqué 
+OfficeFinder.exe "Ceci est ma recherche" ./documentWord.docx
