6847e62281fe7ecc851aef1f81c6a062f90fd503
bhilleri <benjaminh@live.fr>
2024-02-03 20:49:29 +0100

    Test de la recherche dans un fichier word avec une regex

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 ﻿using System.IO.Enumeration;
+using System.Text.RegularExpressions;
 using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+Console.WriteLine("test");
+
 
 using (WordprocessingDocument wordDoc = WordprocessingDocument.Open("test.docx", false)){
-    Console.WriteLine("Hello world");
+    MainDocumentPart mainDocument= wordDoc.MainDocumentPart;
+    Body body = mainDocument.Document.Body;
+    string text = body.InnerText;
+
+    string pattern = @"test";
+    RegexOptions options = RegexOptions.Multiline;
+    
+    foreach (Match m in Regex.Matches(text, pattern, options))
+    {
+        Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+    }
 }
